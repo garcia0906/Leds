09ddbaa26cd1f799398128b71d4b9958969e7329
garcia0906 <127440368+garcia0906@users.noreply.github.com>
2023-05-08 18:51:17 -0500

    Add files via upload

---
+++ ledss/src/main/java/co/edu/manizales/ledss/LedssApplication.java
@@ -0,0 +1,13 @@
+package co.edu.manizales.ledss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedssApplication.class, args);
+    }
+
+}
---
+++ ledss/src/test/java/co/edu/manizales/ledss/LedssApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.manizales.ledss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
